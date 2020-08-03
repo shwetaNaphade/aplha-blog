f6546b87525776a438480fe3e4cc0d84ce28ac12
shwetaNaphade <shweta.naphade12@gmail.com>
2020-08-03 17:07:32 +0530

    added error msgs using flash

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,13 +9,20 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @new_article = Article.new
   end
 
   def create
     @new_article = Article.new(params.require(:article).permit(:title, :description))
-    @new_article.save
-    redirect_to article_path(@new_article)
-      # redirect_to @new_article - this works too 
+
+    if @new_article.save
+      flash[:notice] = "Article was saved successfully."
+      redirect_to article_path(@new_article)
+      # redirect_to @new_article - this works too
+    else
+      render 'new'
+    end
+
   end
 
 end
